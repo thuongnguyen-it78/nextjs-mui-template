ed21bf0f0733eff71ee750b8c97310299bed4357
Andres Nguyen <thuongnguyen.it78@gmail.com>
2023-02-03 16:19:24 +0700

    add date util

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 export const formatDateToString = (string, level = "lv1") => {
   const date = dayjs(string);
 
@@ -11,4 +13,24 @@ export const formatDateToString = (string, level = "lv1") => {
     default:
       return date.format("DD/MM/YYYY");
   }
+};
+
+export const formatTimeAgo = (time) => {
+  const now = dayjs();
+  const timeDayjs = dayjs(time);
+  let ago = now.diff(timeDayjs, "day");
+  if (ago > 2) {
+    return timeDayjs.dayjs("DD-MM-YYYY HH:mm");
+  }
+
+  if (ago > 0) {
+    return `${ago} ngày trước`;
+  }
+
+  ago = now.diff(timeDayjs, "hour");
+  if (ago > 0) {
+    return `${ago} giờ trước`;
+  }
+
+  return `${now.diff(timeDayjs, "minute")} phút trước`;
 };
